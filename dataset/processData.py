1d46b390b73b22e3acfa78631c96ce8d7b5d1ace
Evan Sklarski <esklarski@gmail.com>
2020-04-02 00:23:39 -0700

    trim script of unused lines

--- dataset/processData.py
+++ dataset/processData.py
@@ -16,56 +16,6 @@ stateTranslation = [
   ['Quebec', 'QC'],
   ['Saskatchewan', 'SK'],
   ['Yukon', 'YT'],
-  # ['Arizona', 'AZ'],
-  # ['Alabama', 'AL'],
-  # ['Alaska', 'AK'],
-  # ['Arkansas', 'AR'],
-  # ['California', 'CA'],
-  # ['Colorado', 'CO'],
-  # ['Connecticut', 'CT'],
-  # ['Delaware', 'DE'],
-  # ['Florida', 'FL'],
-  # ['Georgia', 'GA'],
-  # ['Hawaii', 'HI'],
-  # ['Idaho', 'ID'],
-  # ['Illinois', 'IL'],
-  # ['Indiana', 'IN'],
-  # ['Iowa', 'IA'],
-  # ['Kansas', 'KS'],
-  # ['Kentucky', 'KY'],
-  # ['Louisiana', 'LA'],
-  # ['Maine', 'ME'],
-  # ['Maryland', 'MD'],
-  # ['Massachusetts', 'MA'],
-  # ['Michigan', 'MI'],
-  # ['Minnesota', 'MN'],
-  # ['Mississippi', 'MS'],
-  # ['Missouri', 'MO'],
-  # ['Montana', 'MT'],
-  # ['Nebraska', 'NE'],
-  # ['Nevada', 'NV'],
-  # ['New Hampshire', 'NH'],
-  # ['New Jersey', 'NJ'],
-  # ['New Mexico', 'NM'],
-  # ['New York', 'NY'],
-  # ['North Carolina', 'NC'],
-  # ['North Dakota', 'ND'],
-  # ['Ohio', 'OH'],
-  # ['Oklahoma', 'OK'],
-  # ['Oregon', 'OR'],
-  # ['Pennsylvania', 'PA'],
-  # ['Rhode Island', 'RI'],
-  # ['South Carolina', 'SC'],
-  # ['South Dakota', 'SD'],
-  # ['Tennessee', 'TN'],
-  # ['Texas', 'TX'],
-  # ['Utah', 'UT'],
-  # ['Vermont', 'VT'],
-  # ['Virginia', 'VA'],
-  # ['Washington', 'WA'],
-  # ['West Virginia', 'WV'],
-  # ['Wisconsin', 'WI'],
-  # ['Wyoming', 'WY'],
 ]
 
 stateDict = {}
@@ -101,7 +51,7 @@ def processDate(date):
   df = df.apply( translateState, axis=1 )
 
 
-  #print(df['Province_State'].str.contains('Diamond Princess'))
+  # print(df['Province_State'].str.contains('Diamond Princess'))
   stateData = df.groupby(['Country_Region', 'Province_State']).agg('sum').reset_index()
   stateData = stateData[ stateData["Country_Region"] == "Canada" ]
 
