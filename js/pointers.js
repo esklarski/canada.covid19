9db15ce4429f9f3304c1af1c1e3bf93e996cb1c0
Evan Sklarski <esklarski@gmail.com>
2020-04-03 00:59:55 -0700

    revert location href of datasets

--- js/pointers.js
+++ js/pointers.js
@@ -2,7 +2,7 @@ var JHUsource = formatJHUsource();
 
 function formatJHUsource() {
     var temp = _dateUpdated.split('/');
-    return "dataset/jhu-data.csv?d=" + temp[2] + temp[0] + temp[1]
+    return "https://raw.githubusercontent.com/esklarski/canada.covid19/c0b17449031ff1e3f60bf8f9cba3c0e03b84164a/dataset/jhu-data.csv?d=" + temp[2] + temp[0] + temp[1]
 }
 
-var POPsource = "wikipedia-population.csv";
+var POPsource = "https://raw.githubusercontent.com/esklarski/canada.covid19/c0b17449031ff1e3f60bf8f9cba3c0e03b84164a/dataset/wikipedia-population.csv"
