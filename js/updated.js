284508f4b5e7d4d58e05bc4db5297c43f7c3b0af
Evan Sklarski <esklarski@gmail.com>
2020-04-03 18:00:10 -0700

    update for 2020/04/03

--- js/updated.js
+++ js/updated.js
@@ -1,5 +1,5 @@
-var _dateUpdated = "04/02/2020";
-var _timeUpdated = "17:30h"
+var _dateUpdated = "04/03/2020";
+var _timeUpdated = "18:00h"
 
 function updateSpan() {
     document.getElementById("update-date").textContent = _dateUpdated;
