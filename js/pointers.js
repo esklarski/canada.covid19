bb4d49700a7a96cafaf9b61284ca70d3dabe7fde
Evan Sklarski <esklarski@gmail.com>
2020-04-03 18:22:50 -0700

    pointers...

--- js/pointers.js
+++ js/pointers.js
@@ -2,7 +2,7 @@ var JHUsource = formatJHUsource();
 
 function formatJHUsource() {
     var temp = _dateUpdated.split('/');
-    return "https://github.com/esklarski/canada.covid19/blob/master/dataset/jhu-data.csv?d=" + temp[2] + temp[0] + temp[1]
+    return "dataset/jhu-data.csv?d=" + temp[2] + temp[0] + temp[1]
 }
 
-var POPsource = "https://github.com/esklarski/canada.covid19/blob/master/dataset/wikipedia-population.csv"
+var POPsource = "dataset/wikipedia-population.csv"
