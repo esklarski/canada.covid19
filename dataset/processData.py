a70912bee132870d6a8ec71eb4373c73759de3ea
Evan Sklarski <esklarski@gmail.com>
2020-04-09 15:39:17 -0700

    commenting for clarity as I learn

--- dataset/processData.py
+++ dataset/processData.py
@@ -1,7 +1,8 @@
-
+## == define JHU data location == ##
 path = '../../data/'
 
 
+## == province_state translation == ##
 stateTranslation = [
   # ['Alberta', 'AB'],
   # ['British Columbia', 'BC'],
@@ -24,6 +25,7 @@ for el in stateTranslation:
   stateDict[ el[1] ] = el[0]
 
 
+## == translate inconsistent provine_state names == ##
 def translateState(row):
   state = str(row["Province_State"]).strip()
   if ("," in state):
@@ -36,51 +38,73 @@ def translateState(row):
 
   return row
 
+
+## == process file == ##
 def processDate(date):
   print(date)
+
+  # read file
   df = pd.read_csv(path + date + ".csv")
 
+  # rename column headings ** why? **
   if 'Country/Region' in df:
     df = df.rename(columns={
       'Country/Region': 'Country_Region',
       'Province/State': 'Province_State'
     })
 
+  # remove cruise ship entries
   df = df[ df['Province_State'].str.contains('Diamond Princess') != True ]
   df = df[ df['Province_State'].str.contains('Grand Princess') != True ]
 
+  # translate province_state names
   df = df.apply( translateState, axis=1 )
 
+  # ? group and sum provincial data ?
   stateData = df.groupby(['Country_Region', 'Province_State']).agg('sum').reset_index()
   stateData = stateData[ stateData["Country_Region"] == "Canada" ]
 
+  # ? group and sum country data ?
   countrydata = df.groupby(['Country_Region']).agg('sum').reset_index()
   countrydata['Province_State'] = ""
 
+  # ? append country data ?
   df = stateData.append( countrydata )
+
+  # calculate 'active' cases if not provided
   if 'Active' not in df:
     df['Active'] = df['Confirmed'] - df['Recovered'] - df['Deaths']
+
+  # ? add column names ?
   df = df[ ["Country_Region", "Province_State", "Confirmed", "Recovered", "Active", "Deaths"] ] 
   df["Date"] = date
 
   return df
 
 
-
+## == main process == ##
 import pandas as pd
 import os
 
+# create output storage variable
 df = pd.DataFrame()
+
+# pull in files and sort
 files = os.listdir(path)
 files.sort()
+
 for filename in files:
+  # only take .csv files
   if not filename.endswith(".csv"): continue
+
+  # grab file date from file name
   date = filename[0:10]
 
+  # process and append file to output
   df = df.append(processDate(date))
 
 
-# == replace country name to match population ==
+## == replace country name to match population == ##
 countryReplacement = {
   "US": "United States",
   "Korea, South": "South Korea",
@@ -112,7 +136,8 @@ for key in countryReplacement:
   new = countryReplacement[key]
   df["Country_Region"] = df["Country_Region"].replace(old, new)
 
-# == Fix state names ==
+
+## == Fix state names == ##
 stateReplacement = {
   "United States Virgin Islands": "Virgin Islands"
 }
@@ -122,7 +147,8 @@ for key in stateReplacement:
   new = stateReplacement[key]
   df["Province_State"] = df["Province_State"].replace(old, new)
 
-# == Remove 'Recovered' province entry ==
+
+## == Remove 'Recovered' province entry == ##
 entryRemoval = {
   "Recovered": "Recovered",
 }
@@ -130,8 +156,10 @@ entryRemoval = {
 for key in entryRemoval:
   df = df[ df['Province_State'].str.contains(entryRemoval[key]) != True ]
 
-# == Add Population ==
+
+## == Add Population == ##
 df = df.astype({"Confirmed": "int32", "Recovered": "int32", "Active": "int32", "Deaths": "int32"})
 
-#print(df)
+
+## == print(df) to file == ##
 df.to_csv('jhu-data-delta.csv', index=False)
