05188f96c574362e80ed4180bd84c191c17e7f2d
Evan Sklarski <esklarski@gmail.com>
2020-04-09 00:01:39 -0700

    Correct filtering for province naming inconsistencies. Manual tweaking for data file now unnecesary.

--- dataset/processData.py
+++ dataset/processData.py
@@ -3,19 +3,19 @@ path = '../../data/'
 
 
 stateTranslation = [
-  ['Alberta', 'AB'],
-  ['British Columbia', 'BC'],
-  ['Manitoba', 'MB'],
-  ['New Brunswick', 'NB'],
-  ['Newfoundland and Labrador', 'NL'],
-  ['Northwest Territories', 'NT'],
-  ['Nova Scotia', 'NS'],
-  ['Nunavut', 'NU'],
+  # ['Alberta', 'AB'],
+  # ['British Columbia', 'BC'],
+  # ['Manitoba', 'MB'],
+  # ['New Brunswick', 'NB'],
+  # ['Newfoundland and Labrador', 'NL'],
+  # ['Northwest Territories', 'NT'],
+  # ['Nova Scotia', 'NS'],
+  # ['Nunavut', 'NU'],
   ['Ontario', 'ON'],
-  ['Prince Edward Island', 'PE'],
+  # ['Prince Edward Island', 'PE'],
   ['Quebec', 'QC'],
-  ['Saskatchewan', 'SK'],
-  ['Yukon', 'YT'],
+  # ['Saskatchewan', 'SK'],
+  # ['Yukon', 'YT'],
 ]
 
 stateDict = {}
@@ -27,8 +27,8 @@ for el in stateTranslation:
 def translateState(row):
   state = str(row["Province_State"]).strip()
   if ("," in state):
-    if state == "Virgin Islands, U.S.":
-      row["Province_State"] = "Virgin Islands"
+    if state == "Calgary, Alberta" or state == "Edmonton, Alberta":
+      row["Province_State"] = "Alberta"
     else:
       stateCode = state[-2:]
       if stateCode in stateDict:
