5a1d8a988c9f127bd0de6024f40a62fa1ea36781
Evan Sklarski <esklarski@gmail.com>
2020-04-14 21:17:44 -0700

    update for JHU data update 2020/04/14

--- js/updated.js
+++ js/updated.js
@@ -1,5 +1,5 @@
 var _dateUpdated = "2020/04/14";
-var _timeUpdated = "18:45h"
+var _timeUpdated = "21:15h"
 
 function updateSpan() {
     document.getElementById("update-date").textContent = _dateUpdated;
