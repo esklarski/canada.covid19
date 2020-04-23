0b7e926c33e0a730dff93e90cb11853df450a742
Evan Sklarski <esklarski@gmail.com>
2020-04-23 16:50:45 -0700

    misc update - switch to merged-dataset.csv

--- js/pointers.js
+++ js/pointers.js
@@ -2,7 +2,7 @@ var JHUsource = JHUsource();
 
 function JHUsource() {
     var temp = _dateUpdated.split('/');
-    return "dataset/jhu-data.csv?d=" + temp[0] + temp[1] + temp[2];
+    return "dataset/merged-data.csv?d=" + temp[0] + temp[1] + temp[2];
 }
 
 var POPsource = "dataset/wikipedia-population.csv";
