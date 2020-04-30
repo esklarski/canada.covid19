8a4f487d059fbc3f77eb35d1857b295d32758822
Evan Sklarski <esklarski@gmail.com>
2020-04-29 23:35:50 -0700

    set select indicies on page load

--- js/updated.js
+++ js/updated.js
@@ -2,6 +2,11 @@ var _dateUpdated = "2020/04/29";
 var _timeUpdated = "22:45h"
 
 function updateSpan() {
+    // update updated time
     document.getElementById("update-date").textContent = _dateUpdated;
     document.getElementById("update-time").textContent = _timeUpdated;
+
+    // set all select elements to default index
+    Array.from( document.getElementsByClassName("data-select") )
+         .forEach( function(select) { select.selectedIndex = "0"; } )
 }
