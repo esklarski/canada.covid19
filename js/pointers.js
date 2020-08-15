d426e638ac993abc038ecd34ba774221a5445822
Evan Sklarski <esklarski@gmail.com>
2020-08-15 01:01:42 -0700

    update data pointers

--- js/pointers.js
+++ js/pointers.js
@@ -1,5 +1,5 @@
-var JHUsource =  "https://raw.githubusercontent.com/esklarski/canada.covid19/master/dataset/merged-data.csv" + "?d=" + _dateUpdated;
+var JHUsource =  "https://raw.githubusercontent.com/esklarski/canada.covid19/master/dataset/jhu-data.csv" + "?d=" + _dateUpdated;
 
 var POPsource = "https://raw.githubusercontent.com/esklarski/canada.covid19/master/dataset/wikipedia-population.csv";
 
-var C19Csource =  "https://raw.githubusercontent.com/esklarski/canada.covid19/master/dataset/c19c-data.csv" + "?d=" + _dateUpdated;
+var C19Csource =  "https://raw.githubusercontent.com/esklarski/canada.covid19/master/dataset/odwg-data.csv" + "?d=" + _dateUpdated;
