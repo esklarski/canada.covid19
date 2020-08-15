f2005600b6089cf0847fa95566da2d4f23c846e5
Evan Sklarski <esklarski@gmail.com>
2020-08-15 10:03:37 -0700

    correct date format for dataset loading

--- js/pointers.js
+++ js/pointers.js
@@ -1,5 +1,10 @@
-var JHUsource =  "https://raw.githubusercontent.com/esklarski/canada.covid19/master/dataset/jhu-data.csv" + "?d=" + _dateUpdated;
+var temp = _JHUupdated.split('/');
+var JHUdate = temp[1] + "-" + temp[2] + "-" + temp[0];
+temp = _ODWGupdated.split("/");
+var ODWGdate = temp[1] + "-" + temp[2] + "-" + temp[0];
+
+var JHUsource =  "https://raw.githubusercontent.com/esklarski/canada.covid19/master/dataset/jhu-data.csv" + "?d=" + JHUdate;
 
 var POPsource = "https://raw.githubusercontent.com/esklarski/canada.covid19/master/dataset/wikipedia-population.csv";
 
-var ODWGsource =  "https://raw.githubusercontent.com/esklarski/canada.covid19/master/dataset/odwg-data.csv" + "?d=" + _dateUpdated;
+var ODWGsource =  "https://raw.githubusercontent.com/esklarski/canada.covid19/master/dataset/odwg-data.csv" + "?d=" + ODWGdate;
