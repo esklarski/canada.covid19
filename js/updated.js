f266cceded559d50474764de4b00b67ce65b2722
Evan Sklarski <esklarski@gmail.com>
2020-08-16 01:14:55 -0700

    update JHU data from 2020/08/14

--- js/updated.js
+++ js/updated.js
@@ -1,5 +1,5 @@
-var _dateUpdated = "2020/08/15";
-var _timeUpdated = "18:00h";
+var _dateUpdated = "2020/08/16";
+var _timeUpdated = "01:15h";
 var _JHUupdated  = "2020/08/14";
 var _ODWGupdated = "2020/08/15";
 
