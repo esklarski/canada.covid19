4d817f217f33b017b1a147d4bdfc941a576c1508
Evan Sklarski <esklarski@gmail.com>
2020-09-04 21:19:53 -0700

    update ODWG data for 2020/09/04

--- js/updated.js
+++ js/updated.js
@@ -1,6 +1,6 @@
-var _dateUpdated = "2020/09/03";
-var _timeUpdated = "23:00h";
-var _ODWGupdated = "2020/09/03";
+var _dateUpdated = "2020/09/04";
+var _timeUpdated = "21:25h";
+var _ODWGupdated = "2020/09/04";
 var _JHUupdated  = "2020/09/03";
 
 function initPage() {
