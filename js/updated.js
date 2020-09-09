1f60e2f23c4e9cfdc8845239cc548e39b6041f6c
Evan Sklarski <esklarski@gmail.com>
2020-09-08 22:57:37 -0700

    update for 2020/09/08

--- js/updated.js
+++ js/updated.js
@@ -1,7 +1,7 @@
 var _dateUpdated = "2020/09/08";
-var _timeUpdated = "00:30h";
-var _ODWGupdated = "2020/09/07";
-var _JHUupdated  = "2020/09/07";
+var _timeUpdated = "23:00h";
+var _ODWGupdated = "2020/09/08";
+var _JHUupdated  = "2020/09/08";
 
 function initPage() {
     // update updated time
