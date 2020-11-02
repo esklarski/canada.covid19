f4470be0a3cdd9a3a105755d91c322ab860e3019
Evan Sklarski <esklarski@gmail.com>
2020-11-01 18:10:39 -0800

    update for 2020/10/31

--- js/updated.js
+++ js/updated.js
@@ -1,7 +1,7 @@
-var _dateUpdated = "2020/10/31";
-var _timeUpdated = "00:15h";
-var _ODWGupdated = "2020/10/30";
-var _JHUupdated  = "2020/10/30";
+var _dateUpdated = "2020/11/01";
+var _timeUpdated = "18:15h";
+var _ODWGupdated = "2020/10/31";
+var _JHUupdated  = "2020/10/31";
 
 function initPage() {
     // update updated time
@@ -14,3 +14,4 @@ function initPage() {
     Array.from( document.getElementsByClassName("yaxis-select") )
          .forEach( function(select) { select.selectedIndex = "0"; } );
 }
+
