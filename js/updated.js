6b10062b691f3b44d3ccb32be8badd0f191e94ca
Evan Sklarski <esklarski@gmail.com>
2020-11-13 23:00:40 -0800

    update for 2020/11/13

--- js/updated.js
+++ js/updated.js
@@ -1,7 +1,7 @@
-var _dateUpdated = "2020/11/12";
-var _timeUpdated = "00:10h";
-var _ODWGupdated = "2020/10/11";
-var _JHUupdated  = "2020/10/11";
+var _dateUpdated = "2020/11/13";
+var _timeUpdated = "23:00h";
+var _ODWGupdated = "2020/10/13";
+var _JHUupdated  = "2020/10/13";
 
 function initPage() {
     // update updated time
