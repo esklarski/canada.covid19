72729d208c4f9b2d81b81b3a1fa46acc9a2fd7be
Evan Sklarski <esklarski@gmail.com>
2020-12-29 22:49:35 -0800

    update for 2020/12/29

--- js/updated.js
+++ js/updated.js
@@ -1,7 +1,7 @@
-var _dateUpdated = "2020/12/28";
-var _timeUpdated = "23:00h";
-var _ODWGupdated = "2020/12/28";
-var _JHUupdated  = "2020/12/28";
+var _dateUpdated = "2020/12/29";
+var _timeUpdated = "22:50h";
+var _ODWGupdated = "2020/12/29";
+var _JHUupdated  = "2020/12/29";
 
 function initPage() {
     // update updated time
