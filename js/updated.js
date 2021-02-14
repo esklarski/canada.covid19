40244a6573e124a765073087f2d78b1a53e61f55
Evan Sklarski <esklarski@gmail.com>
2021-02-14 00:40:16 -0800

    update to 2021/02/13

--- js/updated.js
+++ js/updated.js
@@ -1,7 +1,7 @@
-var _dateUpdated = "2021/02/11";
-var _timeUpdated = "23:20h";
-var _ODWGupdated = "2021/02/11";
-var _JHUupdated  = "2021/02/11";
+var _dateUpdated = "2021/02/14";
+var _timeUpdated = "00:45h";
+var _ODWGupdated = "2021/02/13";
+var _JHUupdated  = "2021/02/13";
 
 function initPage() {
     // update updated time
