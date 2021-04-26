0ba372b1239b7f34dc0bcf71fbf3a122f5cc5893
Evan Sklarski <esklarski@gmail.com>
2021-04-26 00:46:43 -0700

    update for 2021/04/25

--- js/updated.js
+++ js/updated.js
@@ -1,7 +1,7 @@
-var _dateUpdated = "2021/04/24";
-var _timeUpdated = "23:45h";
-var _ODWGupdated = "2021/04/24";
-var _JHUupdated  = "2021/04/24";
+var _dateUpdated = "2021/04/26";
+var _timeUpdated = "00:45h";
+var _ODWGupdated = "2021/04/25";
+var _JHUupdated  = "2021/04/25";
 
 function initPage() {
     // update updated time
