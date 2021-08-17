bf515a36bd6ce0745528043a3d6b5fc63d32f055
Evan Sklarski <esklarski@gmail.com>
2021-08-17 00:02:28 -0700

    update for 2021/08/16

--- js/updated.js
+++ js/updated.js
@@ -1,7 +1,7 @@
-var _dateUpdated = "2021/08/15";
-var _timeUpdated = "23:45h";
-var _ODWGupdated = "2021/08/15";
-var _JHUupdated  = "2021/08/15";
+var _dateUpdated = "2021/08/17";
+var _timeUpdated = "00:00h";
+var _ODWGupdated = "2021/08/16";
+var _JHUupdated  = "2021/08/16";
 
 function initPage() {
     // update updated time
