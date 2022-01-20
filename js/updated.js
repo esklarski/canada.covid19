c553d4c348f6da051ff1c0ec8c5e1cbd583e678a
Evan Sklarski <esklarski@gmail.com>
2022-01-20 01:34:15 -0800

    update for 2022/01/19

--- js/updated.js
+++ js/updated.js
@@ -1,7 +1,7 @@
-var _dateUpdated = "2022/01/18";
-var _timeUpdated = "23:55h";
-var _ODWGupdated = "2022/01/18";
-var _JHUupdated  = "2022/01/18";
+var _dateUpdated = "2022/01/20";
+var _timeUpdated = "01:30h";
+var _ODWGupdated = "2022/01/19";
+var _JHUupdated  = "2022/01/19";
 
 function initPage() {
     // update updated time
