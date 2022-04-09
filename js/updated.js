88cefc1ee31be93e8515a73cb3a3700313090087
Evan Sklarski <esklarski@gmail.com>
2022-04-08 23:06:19 -0700

    update to 2022/04/08

--- js/updated.js
+++ js/updated.js
@@ -1,7 +1,7 @@
-var _dateUpdated = "2022/04/06";
-var _timeUpdated = "22:35h";
-var _ODWGupdated = "2022/04/06";
-var _JHUupdated  = "2022/04/06";
+var _dateUpdated = "2022/04/08";
+var _timeUpdated = "23:05h";
+var _ODWGupdated = "2022/04/08";
+var _JHUupdated  = "2022/04/08";
 
 function initPage() {
     // update updated time
